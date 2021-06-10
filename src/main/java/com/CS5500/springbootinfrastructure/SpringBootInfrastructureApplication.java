23153c698aa65c279bcb643c33a808239df0d335
Ivor <ivor808@gmail.com>
2021-06-09 19:15:56 -0700

    base code

---
+++ src/main/java/com/CS5500/springbootinfrastructure/SpringBootInfrastructureApplication.java
@@ -0,0 +1,13 @@
+package com.CS5500.springbootinfrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInfrastructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInfrastructureApplication.class, args);
+	}
+
+}
