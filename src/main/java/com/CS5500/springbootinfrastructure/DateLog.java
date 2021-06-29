a8b5b934c5f0ada45d173d1f9e1ee09985e90795
kulkarniakash <kulkarni.akash@gmail.com>
2021-06-29 15:38:36 -0400

    set up skeleton for JPA with on table

---
+++ src/main/java/com/CS5500/springbootinfrastructure/DateLog.java
@@ -0,0 +1,58 @@
+package com.CS5500.springbootinfrastructure;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name = "date_log")
+public class DateLog {
+    /*@Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;*/
+
+    @Id
+    private Date dateOf;
+
+    private Integer caloriesIdle;
+
+
+
+    private Timestamp lastUpdate;
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    /*public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }*/
+
+    public Date getDateOf() {
+        return dateOf;
+    }
+
+    public void setDateOf(Date dateOf) {
+        this.dateOf = dateOf;
+    }
+
+    public Integer getCaloriesIdle() {
+        return caloriesIdle;
+    }
+
+    public void setCaloriesIdle(Integer caloriesIdle) {
+        this.caloriesIdle = caloriesIdle;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void timestampLastUpdate() {
+        this.lastUpdate = new Timestamp(System.currentTimeMillis());
+    }
+}
