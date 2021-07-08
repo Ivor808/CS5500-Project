39895096106be453f9253ddf0a4b8281ae464cd0
ks3364a <satwani.k@northeastern.edu>
2021-07-08 10:08:44 +0530

    Adding helper classes

--- src/main/java/com/CS5500/springbootinfrastructure/dao/Jsonparser.java
+++ src/main/java/com/CS5500/springbootinfrastructure/dao/Jsonparser.java
@@ -4,18 +4,95 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.*;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Jsonparser {
     public static void main(String[] args) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        InputStream inputStream = new FileInputStream(new File("C://Users//Karan//Desktop//ALIGN courses//Summer 2021//CS5500//CS5500-Project//src//main//resources//storyline.json"));
-        TypeReference<List<DateLog>> typeReference = new TypeReference<List<DateLog>>() {};
+        InputStream inputStream = new FileInputStream(new File("src//main//resources//storyline.json"));
+        TypeReference<List<DateLog_helper>> typeReference = new TypeReference<>() {};
+        TypeReference<List<Summary_helper>> placeReference = new TypeReference<>() {};
 
-        DateLog[] DateLogList = mapper.readValue(inputStream, DateLog[].class);
+        DateLog_helper[] DateLogList = mapper.readValue(inputStream, DateLog_helper[].class);
 
-        for (DateLog itr : DateLogList){
-            System.out.print("Date is : " + itr.date + "\n");
+        //Summary array
+        Summary_helper[] summ;
+        List<Summary_helper[]> SummaryList = new ArrayList<>();
+
+        //Segment array
+        Segment_helper[] seg;
+        List<Segment_helper[]> SegmentList = new ArrayList<>();
+
+        //Activity array
+        Activity_helper[] act;
+        List<Activity_helper[]> ActivityList = new ArrayList<>();
+
+        int i = 0;
+        for (DateLog_helper itr : DateLogList){
+            //System.out.print("Date is : " + itr.date + "\n" + i);
+
+            //Accessing Summary
+            summ = DateLogList[i].summary;
+            SummaryList.add(summ);
+
+            //Accessing Segment
+            seg = DateLogList[i].segments;
+            SegmentList.add(seg);
+
+            i++;
         }
+
+        //extract items from summary array, change j to access different items
+        for (i = 0; i < SummaryList.size(); i++){
+            Summary_helper[] iter = SummaryList.get(i);
+            if (iter != null) {
+                for (int j = 0; j < iter.length; j++) {
+                    //System.out.print("Place is : " + iter[j].activity + "\n");
+                }
+            }
+        }
+
+        //extract items from segment array, change j to access different items
+        //type is always place or move
+        //also extracting activity from the segment
+        for (i = 0; i < SegmentList.size(); i++){
+            Segment_helper[] iter = SegmentList.get(i);
+            if (iter != null) {
+                for (int j = 0; j < iter.length; j++) {
+                    //System.out.print("Segment is : " + iter[j].type + "\n");
+                    //Accessing activity
+                    act = iter[j].activities;
+                    ActivityList.add(act);
+                }
+            }
+        }
+
+        //use this to access the
+        for (i = 0; i < ActivityList.size(); i++){
+            Activity_helper[] iter = ActivityList.get(i);
+            if (iter != null) {
+                for (int j = 0; j < iter.length; j++) {
+                    System.out.print("Activity is : " + iter[j].activity + "\n");
+
+                    //Setting start and end times for each activity
+                    //the setter here takes in string and converts to SimpleDateFormat
+                    if (iter[j].startTime == null) {
+                        iter[j].setStart_time(null);
+                    } else {
+                        iter[j].setStart_time(iter[j].startTime);}
+
+                    if (iter[j].endTime == null) {
+                        iter[j].setEnd_time(null);
+                    } else {
+                        iter[j].setEnd_time(iter[j].endTime);
+                    }
+
+                }
+            }
+        }
+
     }
 }
--- src/main/java/com/CS5500/springbootinfrastructure/dao/Summary_helper.java
+++ src/main/java/com/CS5500/springbootinfrastructure/dao/Summary_helper.java
@@ -4,14 +4,11 @@ import javax.persistence.*;
 import java.sql.Date;
 import java.sql.Timestamp;
 
-@Entity // This tells Hibernate to make a table out of this class
-@Table(name = "summary")
-public class Summary {
+public class Summary_helper {
     /*@Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer id;*/
 
-    @Id
     public String activity;
 
     public String group;
